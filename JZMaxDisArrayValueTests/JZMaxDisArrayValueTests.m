7cf369864b1fe707d0e2c684b67dfed8f7b0ffd3
jihong zhang <jihong@jihongs-macbook.gateway.2wire.net>
2014-08-11 08:17:48 -0700

    modified

--- JZMaxDisArrayValueTests/JZMaxDisArrayValueTests.m
+++ JZMaxDisArrayValueTests/JZMaxDisArrayValueTests.m
@@ -6,8 +6,8 @@
 //  Copyright (c) 2014 JZ. All rights reserved.
 //
 #import <SenTestingKit/SenTestingKit.h>
-#import <OCHamcrestIOS/OCHamcrestIOS.h>
 #import "JZMaxDisArrayValueTests.h"
+#import "JZViewController.h"
 
 
 @implementation JZMaxDisArrayValueTests
@@ -29,9 +29,6 @@
 
 - (void)testMaxDisArrayValue
 {
-    //assertThat([self.testJZViewController textArrayInput], is(notNilValue()));
-    //assertThat([self.testJZViewController lableMaxSum], is(notNilValue()));
-    
     NSArray *testArray = @[@"20", @"10", @"50", @"5", @"1"];
     STAssertEquals([self.testJZViewController maxDisArrayValueFor:testArray], 71, @"The max value of the array is incorrect");
     
